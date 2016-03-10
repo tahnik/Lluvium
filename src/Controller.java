28acf71d781e10416807195f27a90cc932bfff97
ec14168 <t.mustasin@se14.qmul.ac.uk>
2016-03-10 10:31:34 +0000

    JSON, iTunes, Slide

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,17 @@
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.SubScene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
 import jdk.nashorn.internal.parser.JSONParser;
 
+import javax.imageio.ImageIO;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -16,13 +21,15 @@ import java.net.URL;
  */
 public class Controller{
     @FXML private ImageView right_forecast;
+    @FXML private ImageView main_artwork;
+    @FXML private ImageView forecastArtwork;
 
     @FXML
     public void changeMusicSuggestion(){
         URL url = null;
         String iTunesInfo = "";
         try {
-            url = new URL("https://itunes.apple.com/search?term=jack+johnson");
+            url = new URL("https://itunes.apple.com/search?term=faded");
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
             String input;
             while((input = in.readLine()) != null){
@@ -34,6 +41,15 @@ public class Controller{
         }
         JsonParser iTunes = new JsonParser();
         JsonElement test= iTunes.parse(iTunesInfo);
-        System.out.println(test.getAsJsonObject().get("resultCount"));
+        System.out.println(test.getAsJsonObject().getAsJsonArray("results").get(0).getAsJsonObject());
+        try{
+            String modifiedURL = test.getAsJsonObject().getAsJsonArray("results").get(0).getAsJsonObject().get("artworkUrl100").toString();
+            modifiedURL = modifiedURL.replace("\"", "");
+            URL imageURL = new URL(modifiedURL);
+            BufferedImage artwork = ImageIO.read(imageURL);
+            forecastArtwork.setImage(SwingFXUtils.toFXImage(artwork, null));
+        }catch (Exception e){
+            System.err.println(e);
+        }
     }
 }
